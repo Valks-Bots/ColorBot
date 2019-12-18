76f7d4bda0bffc45a58026b0e81e71f65a4a27a6
valkyrienyanko <sebastianbelle074@gmail.com>
2019-12-18 01:30:08 -0600

    Complete

--- bot.js
+++ bot.js
@@ -1,16 +1,40 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const tokens = require('./tokens.json')
 
 require('dotenv').config()
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+  client.user.setActivity(`${tokens.prefix}help`, {
+    type: 'PLAYING'
+  })
+  console.log(`${client.user.tag} running on ${client.guilds.size} guilds with ${client.users.size} users.`)
+})
 
-client.on('message', msg => {
-  if (msg.content.startsWith('colors')) {
-    msg.reply('Pong!');
+client.on('message', async msg => {
+  if (msg.author.id !== tokens.ownerId)
+    return
+  
+  if (msg.content.startsWith(tokens.prefix + "help")) {
+    await sendEmbed(msg, "Help")
   }
-});
+  
+  if (msg.content.startsWith(tokens.prefix + "colors")) {
+    await sendEmbed(msg, "Yes")
+    
+    for (const [, member] of msg.guild.members) {
+      const color = await tokens.colors[Math.floor(Math.random()*tokens.colors.length)]
+      await member.addRole(color).then(member => console.log(`Added random color to ${member.displayName}`)).catch(console.error)
+    }
+  }
+})
 
-client.login(process.env.BOT_TOKEN);
+function sendEmbed(msg, desc) {
+  msg.channel.send({
+    embed: {
+      description: desc
+    }
+  })
+}
+
+client.login(process.env.BOT_TOKEN)
